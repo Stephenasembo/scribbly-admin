7a8dfdd836f00ebbac59bd9bbf534a85ad7c8dae
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-18 00:07:37 +0300

    Feat(Auth): Add Login Form And Logic
    Only allow users who are admins to access the app

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,83 @@
+import { useNavigate } from "react-router-dom";
+import Button from "./Button"
+import Input from "./Input"
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [keyCounter, setKeyCounter] = useState(0);
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const url = `${baseUrl}auth/login`
+
+  async function sendData() {
+    let response = await fetch(url, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+    if (response.status !== 200) {
+      let errorData = await response.json()
+      return console.log(errorData);
+    }
+    response = await response.json();
+    const user = response.user;
+    if(user.admin) {
+      const jwt = response.token;
+      localStorage.setItem('jwt', `Bearer ${jwt}`)
+      return navigate('/app');
+    }
+    return navigate('/forbidden');
+  }
+
+  function submitForm(e) {
+    e.preventDefault();
+    setKeyCounter(keyCounter + 1);
+    sendData();
+    setFormData({});
+  }
+
+  return(
+    <div>
+      <h1 className='heading'>Login To Your Account</h1>
+      <form action="/" method="post">
+        <p>
+          Fields marked with an asterisk(*) are required.
+        </p>
+        <label htmlFor="username">
+          Username(*): 
+          <Input
+          key={`username${keyCounter}`}
+          type="text"
+          name="username"
+          id="username"
+          placeholder="John Doe"
+          isrequired={true}
+          data={formData}
+          setData={setFormData}
+          />
+        </label>
+        <label htmlFor="password">
+          Password(*): 
+          <Input
+          key={`password${keyCounter}`}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your password"
+          isrequired={true}
+          data={formData}
+          setData={setFormData}
+          />
+        </label>
+        <Button
+        text='Log in'
+        onClick={submitForm}
+        />
+      </form>
+    </div>
+  )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,16 @@
 import Index from "./components/Index";
 import ErrorPage from "./components/ErrorPage";
+import LoginForm from "./components/LoginForm";
 
 const routes = [
   {
     path: '/',
     element: <Index />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: 'login',
+    element: <LoginForm />,
   },
 ]
 
